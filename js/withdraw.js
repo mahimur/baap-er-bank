1ae860ac6262c6a84844493fb27f0e6600323cec
mahimur <hellomahimur@gmail.com>
2022-08-16 23:10:13 +0600

    second

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,8 +3,8 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     // step-2: get the withdraw amount from the withdraw field
     // For input field use .value to get the inside the input field
     const withdrawField = document.getElementById('withdraw-field');
-    const newwithdrawAmountString = withdrawField.value;
-    const newwithdrawAmount = parseFloat(newwithdrawAmountString);
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
     // step-3: get the current withdraw total
     // for non-input (element other that input, textarea) use innerText to get the text
@@ -12,22 +12,27 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const previouswithdrawTotalString = withdrawTotalElement.innerText;
     const previouswithdrawTotal = parseFloat(previouswithdrawTotalString);
 
-    // add numbers to set the withdraw
-    const currentwithdrawTotal = previouswithdrawTotal + newwithdrawAmount;
-    // set the withdraw total
-    withdrawTotalElement.innerText = currentwithdrawTotal;
-
     // step-5: get balance current total
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    // step-7: clear the withdraw field
+    withdrawField.value = '';
+
     // step-6: calculate current total balance
-    const currentBalanceTotal = previousBalanceTotal - newwithdrawAmount;
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baap er Bank e eto taka nai!');
+        return;
+    }
+
+    // step-4: add numbers to set the withdraw
+    const currentwithdrawTotal = previouswithdrawTotal + newWithdrawAmount;
+    // set the withdraw total
+    withdrawTotalElement.innerText = currentwithdrawTotal;
 
     // set the balance total
     balanceTotalElement.innerText = currentBalanceTotal;
-
-    // step-7: clear the withdraw field
-    withdrawField.value = '';
 });
